eaf191801ac87a43d3c0a3622ea87c95d17092d3
13ellow <mikibun.10969@gmail.com>
2024-04-13 15:57:30 +0900

    Create HistoryCard

--- frontend/src/components/Headers.tsx
+++ frontend/src/components/Headers.tsx
@@ -1,33 +1,31 @@
-"use client"
+"use client";
 
-import { Box, Tab } from '@mui/material';
-import {TabList,TabPanel,TabContext} from '@mui/lab';
-import React,{useState} from 'react';
+import { Box, Tab } from "@mui/material";
+import { TabList, TabPanel, TabContext } from "@mui/lab";
+import React, { useState } from "react";
 
-import {Histories} from './components';
+import { Histories } from "./Components";
 
 export const Headers = () => {
-    const [tabValue,setTabValue] = useState(1)
+  const [tabValue, setTabValue] = useState(1);
 
-    const handleChange = (event:React.SyntheticEvent,newValue:number) => {
-        setTabValue(newValue)
-    }
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
-    return (
-        <div>
-            <TabContext value={tabValue}>
-                {/* <Box> */}
-                <TabList onChange={handleChange}>
-                    <Tab label="HISTROY" value="1"/>
-                    <Tab label="ABOUT Me" value="2"/>
-                </TabList>
-                {/* </Box> */}
+  return (
+    <div>
+      <TabContext value={tabValue}>
+        <TabList onChange={handleChange}>
+          <Tab label="HISTROY" value="1" />
+          <Tab label="ABOUT Me" value="2" />
+        </TabList>
 
-                <TabPanel value="1">History
-                    <Histories/>
-                </TabPanel>
-                <TabPanel value="2">About Me</TabPanel>
-            </TabContext>
-        </div>
-    );
-}
+        <TabPanel value="1">
+          <Histories />
+        </TabPanel>
+        <TabPanel value="2">About Me</TabPanel>
+      </TabContext>
+    </div>
+  );
+};
